f28bf313e696138a5fca0e06e10a5086912f4935
josuhun <chosh9128@gmail.com>
2024-06-10 14:29:02 +0900

    Feat : 비동기 로딩페이지 생성

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,27 @@
 import Nav from "../components/common/Nav";
-import IntroSection from "../components/Home/IntroSection";
-import InfoSection from "../components/Home/InfoSection";
 import SkillSection from "../components/Home/SkillSection";
 import ProjectSection from "../components/Home/ProjectSection";
 import Footer from "../components/common/Footer";
 import * as h from "../components/styles/Home";
+import { Suspense, lazy } from "react";
+import Loading from "./Loading";
+
+const IntroSection = lazy(() => import("../components/Home/IntroSection"));
+const InfoSection = lazy(() => import("../components/Home/InfoSection"));
 
 const Home = () => {
   return (
     <>
-      <Nav />
-      <h.HomeBackGround className="home_bg">
-        <IntroSection />
-        <InfoSection />
-        <SkillSection />
-        <ProjectSection />
-        <Footer />
-      </h.HomeBackGround>
+      <Suspense fallback={<Loading />}>
+        <Nav />
+        <h.HomeBackGround className="home_bg">
+          <IntroSection />
+          <InfoSection />
+          <SkillSection />
+          <ProjectSection />
+          <Footer />
+        </h.HomeBackGround>
+      </Suspense>
     </>
   );
 };
