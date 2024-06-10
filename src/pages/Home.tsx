9c0bf1b9d80bafa6f30036e7711e4dac274198c5
josuhun <chosh9128@gmail.com>
2024-06-10 15:44:58 +0900

    Test : 로딩 배포 테스트

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,7 +8,6 @@ import { Suspense, lazy, useEffect, useState } from "react";
 import Loading from "./Loading";
 
 const IntroSection = lazy(() => import("../components/Home/IntroSection"));
-// const InfoSection = lazy(() => import("../components/Home/InfoSection"));
 
 const Home = () => {
   const [progress, setProgress] = useState(0);
@@ -16,7 +15,9 @@ const Home = () => {
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setProgress((prev) => (prev < 90 ? prev + 10 : prev));
+      setProgress((prev) =>
+        prev < 90 ? prev + 10 : prev >= 90 && prev < 99 ? prev + 3 : prev
+      );
     }, 500);
 
     return () => clearInterval(interval);
