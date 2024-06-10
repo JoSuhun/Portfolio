6498198a64e30c292d4ab06128088b04bca8aa85
josuhun <chosh9128@gmail.com>
2024-06-10 22:22:25 +0900

    Fix : 비동기 App으로 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,45 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
+import { Suspense, lazy, useEffect, useState } from "react";
+import Loading from "./components/common/Loading";
+
+const Home = lazy(() => import("./pages/Home"));
 
 function App() {
+  const [progress, setProgress] = useState(0);
+  const [isIntroLoaded, setIsIntroLoaded] = useState(false);
+  const [isInitialLoad, setIsInitialLoad] = useState(true);
+
+  useEffect(() => {
+    if (isInitialLoad) {
+      const interval = setInterval(() => {
+        setProgress((prev) =>
+          prev < 90 ? prev + 10 : prev >= 90 && prev < 99 ? prev + 3 : prev
+        );
+      }, 500);
+
+      return () => clearInterval(interval);
+    }
+  }, [isInitialLoad]);
+
+  useEffect(() => {
+    if (progress >= 100) {
+      setIsIntroLoaded(true);
+    }
+  }, [progress]);
+
+  useEffect(() => {
+    if (isIntroLoaded) {
+      setProgress(100);
+      setIsInitialLoad(false);
+    }
+  }, [isIntroLoaded]);
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <Suspense fallback={<Loading progress={progress} />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Suspense>
     </Router>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,49 +1,22 @@
-import { Suspense, lazy, useEffect, useState } from "react";
-// import Nav from "../components/common/Nav";
-// import InfoSection from "../components/Home/InfoSection";
-// import SkillSection from "../components/Home/SkillSection";
-// import ProjectSection from "../components/Home/ProjectSection";
-// import Footer from "../components/common/Footer";
-// import * as h from "../components/styles/Home";
-import Loading from "../components/common/Loading";
-
-const IntroSection = lazy(() => import("../components/Home/IntroSection"));
+import Nav from "../components/common/Nav";
+import IntroSection from "../components/Home/IntroSection";
+import InfoSection from "../components/Home/InfoSection";
+import SkillSection from "../components/Home/SkillSection";
+import ProjectSection from "../components/Home/ProjectSection";
+import Footer from "../components/common/Footer";
+import * as h from "../components/styles/Home";
 
 const Home = () => {
-  const [progress, setProgress] = useState(0);
-  const [isInitialLoad, setIsInitialLoad] = useState(true);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setProgress((prev) =>
-        prev < 90 ? prev + 10 : prev >= 90 && prev < 99 ? prev + 3 : prev
-      );
-    }, 500);
-
-    return () => clearInterval(interval);
-  }, []);
-
-  useEffect(() => {
-    if (progress >= 99) {
-      setIsInitialLoad(false);
-    }
-  }, [progress]);
-
   return (
     <>
-      {isInitialLoad && <Loading progress={progress} />}
-      {!isInitialLoad && (
-        <Suspense fallback={<Loading progress={progress} />}>
-          {/* <Nav /> */}
-          {/* <h.HomeBackGround className="home_bg"> */}
-          <IntroSection />
-          {/* <InfoSection /> */}
-          {/* <SkillSection /> */}
-          {/* <ProjectSection /> */}
-          {/* <Footer /> */}
-          {/* </h.HomeBackGround> */}
-        </Suspense>
-      )}
+      <Nav />
+      <h.HomeBackGround className="home_bg">
+        <IntroSection />
+        <InfoSection />
+        <SkillSection />
+        <ProjectSection />
+        <Footer />
+      </h.HomeBackGround>
     </>
   );
 };
