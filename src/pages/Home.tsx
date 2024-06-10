6fe59c02797a26587225d96758a5fe934316cd2c
josuhun <chosh9128@gmail.com>
2024-06-10 22:04:18 +0900

    Fix : 배포 테스트

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,10 @@
 import { Suspense, lazy, useEffect, useState } from "react";
-import Nav from "../components/common/Nav";
-import InfoSection from "../components/Home/InfoSection";
-import SkillSection from "../components/Home/SkillSection";
-import ProjectSection from "../components/Home/ProjectSection";
-import Footer from "../components/common/Footer";
-import * as h from "../components/styles/Home";
+// import Nav from "../components/common/Nav";
+// import InfoSection from "../components/Home/InfoSection";
+// import SkillSection from "../components/Home/SkillSection";
+// import ProjectSection from "../components/Home/ProjectSection";
+// import Footer from "../components/common/Footer";
+// import * as h from "../components/styles/Home";
 import Loading from "../components/common/Loading";
 
 const IntroSection = lazy(() => import("../components/Home/IntroSection"));
@@ -25,7 +25,6 @@ const Home = () => {
 
   useEffect(() => {
     if (progress >= 99) {
-      console.log("ws");
       setIsInitialLoad(false);
     }
   }, [progress]);
@@ -35,14 +34,14 @@ const Home = () => {
       {isInitialLoad && <Loading progress={progress} />}
       {!isInitialLoad && (
         <Suspense fallback={<Loading progress={progress} />}>
-          <Nav />
-          <h.HomeBackGround className="home_bg">
-            <IntroSection />
-            <InfoSection />
-            <SkillSection />
-            <ProjectSection />
-            <Footer />
-          </h.HomeBackGround>
+          {/* <Nav /> */}
+          {/* <h.HomeBackGround className="home_bg"> */}
+          <IntroSection />
+          {/* <InfoSection /> */}
+          {/* <SkillSection /> */}
+          {/* <ProjectSection /> */}
+          {/* <Footer /> */}
+          {/* </h.HomeBackGround> */}
         </Suspense>
       )}
     </>
