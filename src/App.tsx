f1fa204d75c851a886d3898b92bd7a0d615ee601
josuhun <chosh9128@gmail.com>
2024-06-24 14:59:05 +0900

    Test : 로딩페이지 배포환경 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,35 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import ProjectDetail from "./pages/ProjectDetail";
+import { Suspense, useEffect, useState } from "react";
+import Loading from "./components/common/Loading";
 
 function App() {
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setProgress((prev) =>
+        prev < 90 ? prev + 10 : prev >= 90 && prev < 99 ? prev + 3 : prev
+      );
+    }, 500);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  useEffect(() => {
+    if (progress >= 99) {
+      setProgress(100);
+    }
+  }, [progress]);
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/project/:name" element={<ProjectDetail />} />
-      </Routes>
+      <Suspense fallback={<Loading progress={progress} />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/project/:name" element={<ProjectDetail />} />
+        </Routes>
+      </Suspense>
     </Router>
   );
 }
